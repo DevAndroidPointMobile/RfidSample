7d422410ef8c4c7348fdf4261442402c95161078
characin.choi <characin.choi@pointmobile.com>
2026-01-29 16:31:31 +0900

    apply sdk version 3.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     defaultConfig {
         applicationId = "device.apps.rfidsamplev2"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
